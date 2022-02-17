ea01f8d3673e60bf7451c926afafe38fb91c9319
Dimkabit <bit.82@mail.ru>
2022-02-17 20:53:39 +0300

    add lasr

--- src/pages/product-page/product-page.js
+++ src/pages/product-page/product-page.js
@@ -1,11 +1,16 @@
 import React from "react";
 import styles from "./productPage.module.scss";
+import { MenuTypePage } from "../../components/menuTypePage/menuTypePage";
 
 
-const ProductPage = () => {
+const ProductPage = (page) => {
+
+	const pages = page.page;
+	const productType = pages.toLowerCase();
+	const pageType = page.page + `${page.page}'s tracksuit Q109`;
 	return (
-		<div className={styles.productpage}>
-			
+		<div className={styles.productpage} data-test-id={`product-page-${productType}`}>
+			<MenuTypePage page={pageType}/>
 		</div>
 	)
 };
