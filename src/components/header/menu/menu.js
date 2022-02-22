947b4a2bc7c79c378c253eb59b038303250cc6e5
Dimkabit <bit.82@mail.ru>
2022-02-22 23:09:27 +0300

    fix menu

--- src/components/header/menu/menu.js
+++ src/components/header/menu/menu.js
@@ -11,9 +11,9 @@ const Menu = () => {
 	const hide = () => setIsOpen(false);
 	const show = () => setIsOpen(true);
 	return (
-		<div className="foot-header__menu">
+		<div className="foot-header__menu" >
 			<button type="button" className={`icon-menu ${isOpen ? 'menu-open' : ''}`} onClick={toggle} data-test-id='burger-menu-btn'><span></span></button>
-			<nav className={`menu__body ${isOpen ? 'body-open' : ''}`}>
+			<nav className={`menu__body ${isOpen ? 'body-open' : ''}`} data-test-id='burger-menu'>
 				<ul className="menu__list">
 				<li className="menu__item">
 					{MENU.map(({ id, path, name }) => (
