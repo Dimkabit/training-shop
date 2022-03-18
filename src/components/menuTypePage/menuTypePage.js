76867928772eedffc317acd62466fddc63de7e54
Dimkabit <bit.82@mail.ru>
2022-03-18 19:10:11 +0300

    fix links

--- src/components/menuTypePage/menuTypePage.js
+++ src/components/menuTypePage/menuTypePage.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./menuTypePage.scss";
+import { Link } from "react-router-dom";
 
 const MenuTypePage = (page) => {
 	const typePage = page.page;
+	const nameProduct = page.nameProduct;
 
 	return (
 		 <div className='wrapper-products-page-nav'>
 			  <div className='products-page-nav'>
-					<div className='products-page-nav_page'><span>Home</span> ► {typePage}</div>
+					<div className='products-page-nav_page'><Link to={`/`}><span>Home</span></Link> ► <Link to={`/${typePage}`}><span>{typePage}</span></Link> 
+                {nameProduct ? <span>{`► ${nameProduct}`}</span> : null}</div>
 			  </div>
 		 </div>
 	)
